6abbfcb30767caa577e03b4ff8375ba3af6e2a60
Paul Bailey <paul.m.bailey@gmail.com>
2016-10-01 10:48:39 -0400

    login api

--- views.js
+++ views.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var api = require('express-api-helper');
+var express_api = require('express-api-helper');
+
+var models = require('./models.js');
 
 var home = express.Router();
 var api = express.Router();
@@ -8,18 +10,48 @@ home.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+function error_response(request, response, message) {
+  return function(error) {
+    console.log(error);
+    express_api.serverError(request, response, message);
+  };
+}
+
 api.post('/login', function(request, response) {
   var username = request.body.username;
   if (username) {
-    //todo: get or create
+    var promise = models.User.findOne({
+      'username': username
+    }).exec();
+    promise.then(function(user) {
+      console.log(user);
+      if (!user) {
+        user = new models.User({
+          username: username
+        });
+        var p = user.save();
 
-    // really insecure toke :-)
-    return api.ok(request, response, {
-      token: username
+        p.then(function() {
+          // really insecure token :-)
+          console.log('Created: ', username);
+          express_api.ok(request, response, {
+            token: username
+          });
+        });
+
+        p.catch(error_response('Error creating user.'));
+      } else {
+        // really insecure token :-)
+        express_api.ok(request, response, {
+          token: username
+        });
+      }
     });
-  }
 
-  return api.badRequest(request, response, 'No Username');
+    promise.catch(error_response('Database Error'));
+  } else {
+    return express_api.badRequest(request, response, 'No Username');
+  }
 });
 
 module.exports = {
